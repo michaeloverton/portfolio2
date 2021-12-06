86397b28a0c57caae39d3405d9aeb69684eef810
Michael Overton <m.overtonbrown@gmail.com>
2021-12-06 15:11:37 -0800

    promote phreaker

--- src/Games.tsx
+++ src/Games.tsx
@@ -3,9 +3,15 @@ import is2 from "./assets/images/games/infinite-silence/is2.png";
 import is11 from "./assets/images/games/infinite-silence/is11.png";
 import is6 from "./assets/images/games/infinite-silence/is6.png";
 import isTitle from "./assets/images/games/infinite-silence/big-title.png";
-import ph3 from "./assets/images/games/phreaker/ph3.png";
-import ph6 from "./assets/images/games/phreaker/ph6.png";
+// import ph3 from "./assets/images/games/phreaker/ph3.png";
+// import ph6 from "./assets/images/games/phreaker/ph6.png";
+// import ph7 from "./assets/images/games/phreaker/ph7.png";
+import ph2 from "./assets/images/games/phreaker/phreaker2.png";
+import ph3 from "./assets/images/games/phreaker/phreaker3.png";
 import ph7 from "./assets/images/games/phreaker/ph7.png";
+import ph8 from "./assets/images/games/phreaker/phreaker8.png";
+import phreaker7 from "./assets/images/games/phreaker/phreaker7.png";
+import ph5 from "./assets/images/games/phreaker/phreaker5.png";
 import phTitle from "./assets/images/games/phreaker/phTitle.png";
 import oh1 from "./assets/images/games/clock-out/oh1.png";
 import oh7 from "./assets/images/games/clock-out/oh7.png";
@@ -31,6 +37,71 @@ const Games: React.FC = () => {
       </IntroText>
 
       <SectionContainer borderWidth={borderWidth} top right>
+        <SectionHeader borderWidth={borderWidth}>phreaker</SectionHeader>
+
+        <SectionInfo>
+          Phreaker is a virtual reality grappling hook game set in a large-scale
+          cyberpunk space. More levels are in development, featuring gigantic
+          “enemies”, alternate traversal methods, and a "combat" system. The
+          goal is to ultimately target the Oculus Quest 2 wirelessly, along with
+          all other wired VR platforms.
+        </SectionInfo>
+
+        <Row className="px-2 mb-4">
+          <Col>
+            <iframe
+              title="Phreaker on itch.io"
+              src="https://itch.io/embed/1266372?bg_color=0f1117&amp;fg_color=15ff00&amp;link_color=ffe800&amp;border_color=15ff00"
+              width="100%"
+              height="167"
+            >
+              <a href="https://phasein.itch.io/phreaker">Phreaker by phasein</a>
+            </iframe>
+          </Col>
+        </Row>
+
+        {/* External links */}
+        <Row className="px-2 pb-2">
+          <Col lg={6}>
+            <ExternalLink url="https://phasein.itch.io/phreaker">
+              download
+            </ExternalLink>
+          </Col>
+        </Row>
+
+        <SectionHeader rowClass="mb-4" borderWidth={borderWidth} />
+
+        <Row className="mb-3">
+          <Col>
+            <iframe
+              width="100%"
+              height="450"
+              src="https://www.youtube.com/embed/E6oWN7xVrCM"
+              title="YouTube video player"
+              allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            ></iframe>
+          </Col>
+        </Row>
+
+        <GameImage src={phTitle} />
+
+        <GameImage src={ph2} />
+
+        <GameImage src={ph3} />
+
+        <GameImage src={ph7} />
+
+        <GameImage src={phreaker7} />
+
+        <GameImage src={ph8} />
+
+        <GameImage src={ph5} />
+
+        {/* SPACER */}
+        <div className="spacer"></div>
+      </SectionContainer>
+
+      <SectionContainer borderWidth={borderWidth} top left bottom>
         <SectionHeader borderWidth={borderWidth}>
           infinite silence
         </SectionHeader>
@@ -94,65 +165,6 @@ const Games: React.FC = () => {
             ></iframe>
           </Col>
         </Row>
-
-        {/* SPACER */}
-        <div className="spacer"></div>
-      </SectionContainer>
-
-      <SectionContainer borderWidth={borderWidth} top left bottom>
-        <SectionHeader borderWidth={borderWidth}>phreaker</SectionHeader>
-
-        <SectionInfo>
-          Phreaker is a virtual reality grappling hook game set in a large-scale
-          cyberpunk space. More levels are in development, featuring gigantic
-          “enemies”, alternate traversal methods, and a "combat" system. The
-          goal is to ultimately target the Oculus Quest 2 wirelessly, along with
-          all other wired VR platforms.
-        </SectionInfo>
-
-        <Row className="px-2 mb-4">
-          <Col>
-            <iframe
-              title="Phreaker on itch.io"
-              src="https://itch.io/embed/1266372?bg_color=0f1117&amp;fg_color=15ff00&amp;link_color=ffe800&amp;border_color=15ff00"
-              width="100%"
-              height="167"
-            >
-              <a href="https://phasein.itch.io/phreaker">Phreaker by phasein</a>
-            </iframe>
-          </Col>
-        </Row>
-
-        {/* External links */}
-        <Row className="px-2 pb-2">
-          <Col lg={6}>
-            <ExternalLink url="https://phasein.itch.io/phreaker">
-              download
-            </ExternalLink>
-          </Col>
-        </Row>
-
-        <SectionHeader rowClass="mb-4" borderWidth={borderWidth} />
-
-        <Row className="mb-3">
-          <Col>
-            <iframe
-              width="100%"
-              height="450"
-              src="https://www.youtube.com/embed/E6oWN7xVrCM"
-              title="YouTube video player"
-              allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            ></iframe>
-          </Col>
-        </Row>
-
-        <GameImage src={phTitle} />
-
-        <GameImage src={ph3} />
-
-        <GameImage src={ph6} />
-
-        <GameImage src={ph7} />
 
         {/* SPACER */}
         <div className="spacer"></div>
